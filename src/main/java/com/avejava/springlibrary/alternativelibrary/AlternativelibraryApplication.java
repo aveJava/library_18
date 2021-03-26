b9a58c2c7e75331cccf868bb47d34045ac0aaaf9
aveJava <javatalk@mail.ru>
2021-03-26 23:07:26 +0300

    Initial commit

---
+++ src/main/java/com/avejava/springlibrary/alternativelibrary/AlternativelibraryApplication.java
@@ -0,0 +1,13 @@
+package com.avejava.springlibrary.alternativelibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlternativelibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlternativelibraryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/avejava/springlibrary/alternativelibrary/AlternativelibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avejava.springlibrary.alternativelibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlternativelibraryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
