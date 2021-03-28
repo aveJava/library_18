3b9af9d2062747f11c6019bab332edab7d7976ef
aveJava <javatalk@mail.ru>
2021-03-28 21:42:13 +0300

    added Service layer

---
+++ src/main/java/com/avejava/springlibrary/alternativelibrary/service/AuthorEntityService.java
@@ -0,0 +1,60 @@
+package com.avejava.springlibrary.alternativelibrary.service;
+
+import com.avejava.springlibrary.alternativelibrary.dao.AuthorEntityDao;
+import com.avejava.springlibrary.alternativelibrary.domain.AuthorEntity;
+import com.avejava.springlibrary.alternativelibrary.repository.AuthorEntityRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+
+import java.util.List;
+
+public class AuthorEntityService implements AuthorEntityDao {
+    @Autowired
+    AuthorEntityRepo authorRepo;
+
+    @Override
+    public List<AuthorEntity> getAll() {
+        return authorRepo.findAll();
+    }
+
+    @Override
+    public AuthorEntity get(long id) {
+        return authorRepo.getOne(id);
+    }
+
+    @Override
+    public AuthorEntity save(AuthorEntity obj) {
+        return authorRepo.save(obj);
+    }
+
+    @Override
+    public void delete(AuthorEntity object) {
+        authorRepo.delete(object);
+    }
+
+    @Override
+    public List<AuthorEntity> search(String ... searchString) {
+        return authorRepo.findByFioContainingIgnoreCaseOrderByFio(searchString[0]);
+    }
+
+    @Override
+    public List<AuthorEntity> getAll(Sort sort) {
+        return authorRepo.findAll(sort);
+    }
+
+    @Override
+    public Page<AuthorEntity> getAll(int pageNumber, int pageSize, String sortField, Sort.Direction sortDirection) {
+        Sort sort = Sort.by(sortDirection, sortField);
+        PageRequest pageRequest = PageRequest.of(pageNumber, pageSize, sort);
+        return authorRepo.findAll(pageRequest);
+    }
+
+    @Override
+    public Page<AuthorEntity> search(int pageNumber, int pageSize, String sortField, Sort.Direction sortDirection, String... searchString) {
+        Sort sort = Sort.by(sortDirection, sortField);
+        PageRequest pageRequest = PageRequest.of(pageNumber, pageSize, sort);
+        return authorRepo.findByFioContainingIgnoreCaseOrderByFio(searchString[0], pageRequest);
+    }
+}
---
+++ src/main/java/com/avejava/springlibrary/alternativelibrary/service/PublisherEntityService.java
@@ -0,0 +1,60 @@
+package com.avejava.springlibrary.alternativelibrary.service;
+
+import com.avejava.springlibrary.alternativelibrary.dao.PublisherEntityDao;
+import com.avejava.springlibrary.alternativelibrary.domain.PublisherEntity;
+import com.avejava.springlibrary.alternativelibrary.repository.PublisherEntityRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+
+import java.util.List;
+
+public class PublisherEntityService implements PublisherEntityDao {
+    @Autowired
+    PublisherEntityRepo publisherRepo;
+
+    @Override
+    public List<PublisherEntity> getAll() {
+        return publisherRepo.findAll();
+    }
+
+    @Override
+    public PublisherEntity get(long id) {
+        return publisherRepo.getOne(id);
+    }
+
+    @Override
+    public PublisherEntity save(PublisherEntity obj) {
+        return publisherRepo.save(obj);
+    }
+
+    @Override
+    public void delete(PublisherEntity object) {
+        publisherRepo.delete(object);
+    }
+
+    @Override
+    public List<PublisherEntity> search(String... searchString) {
+        return publisherRepo.findByNameContainingIgnoreCaseOrderByName(searchString[0]);
+    }
+
+    @Override
+    public List<PublisherEntity> getAll(Sort sort) {
+        return publisherRepo.findAll(sort);
+    }
+
+    @Override
+    public Page<PublisherEntity> getAll(int pageNumber, int pageSize, String sortField, Sort.Direction sortDirection) {
+        Sort sort = Sort.by(sortDirection, sortField);
+        PageRequest pageRequest = PageRequest.of(pageNumber, pageSize, sort);
+        return publisherRepo.findAll(pageRequest);
+    }
+
+    @Override
+    public Page<PublisherEntity> search(int pageNumber, int pageSize, String sortField, Sort.Direction sortDirection, String... searchString) {
+        Sort sort = Sort.by(sortDirection, sortField);
+        PageRequest pageRequest = PageRequest.of(pageNumber, pageSize, sort);
+        return publisherRepo.findByNameContainingIgnoreCaseOrderByName(searchString[0], pageRequest);
+    }
+}
