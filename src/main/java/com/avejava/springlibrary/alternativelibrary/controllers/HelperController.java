2e9d246845a43dcadf22c8b51da3cb358840f662
mmukhin <javatalk@mail.ru>
2021-04-09 13:43:44 +0300

    added footer of books in the library (author name, views, rating ). improved html, css. added new methods HelperController toolbarButtonListener and toolbarPageSizeListener

--- src/main/java/com/avejava/springlibrary/alternativelibrary/controllers/HelperController.java
+++ src/main/java/com/avejava/springlibrary/alternativelibrary/controllers/HelperController.java
@@ -31,4 +31,44 @@ public class HelperController {
         return "";
     }
 
+    // Toolbar_NumberButtons-servlet - слушает кнопки тулбара на главной странице
+    @GetMapping("/Toolbar/NumberButtons")
+    public String toolbarButtonListener(@RequestParam("title") String title) {
+        switch (title) {
+            case "<<":
+                MainPageController.pageNumber = 1;
+                break;
+            case "<":
+                if (MainPageController.pageNumber > 1) MainPageController.pageNumber--;
+                break;
+            case ">":
+                MainPageController.pageNumber++;
+                break;
+            case ">>":
+                MainPageController.pageNumber = MainPageController.maxPageNumber;
+                break;
+        }
+        return "redirect:/";
+    }
+
+    // Toolbar_PageSize-servlet - слушает кнопки тулбара на главной странице
+    @GetMapping("/Toolbar/PageSize")
+    public String toolbarPageSizeListener(@RequestParam("size") int size) {
+        switch (size) {
+            case 5:
+                MainPageController.pageSize = 5;
+                break;
+            case 10:
+                MainPageController.pageSize = 10;
+                break;
+            case 15:
+                MainPageController.pageSize = 15;
+                break;
+            case 20:
+                MainPageController.pageSize = 20;
+                break;
+        }
+        return "redirect:/";
+    }
+
 }
--- src/main/java/com/avejava/springlibrary/alternativelibrary/controllers/MainPageController.java
+++ src/main/java/com/avejava/springlibrary/alternativelibrary/controllers/MainPageController.java
@@ -4,6 +4,8 @@ import com.avejava.springlibrary.alternativelibrary.domain.BookEntity;
 import com.avejava.springlibrary.alternativelibrary.domain.GenreEntity;
 import com.avejava.springlibrary.alternativelibrary.service.BookEntityService;
 import com.avejava.springlibrary.alternativelibrary.service.GenreEntityService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,22 +15,45 @@ import java.util.List;
 
 @Controller
 public class MainPageController {
+    /** Инстансы сервисов */
     BookEntityService bookService;
     GenreEntityService genreService;
+
+    List<BookEntity> topBooks;
+
+    /** Содержимое библиотеки */
+    static int pageNumber;      // номер текущей страницы (начиная с 1)
+    static int pageSize;        // кол-во книг на одной странице
+    static int maxPageNumber;   // сколько всего страниц
+    static long totalElements;   // сколько всего элементов (на всех страницах)
 
     public MainPageController(BookEntityService bookService, GenreEntityService genreService) {
         this.bookService = bookService;
         this.genreService = genreService;
+
+        topBooks = bookService.findTopBooks(5);
+        pageNumber = 1;
+        pageSize = 10;
     }
 
     @GetMapping("/")
     public String baseUrlRedirect(Model model) {
-        List<BookEntity> topBooks = bookService.findTopBooks(5);
+        // Содержимое полки
         model.addAttribute("top", topBooks);
+        // Содержимое меню жанров
         List<GenreEntity> genres = genreService.getAll();
         model.addAttribute("allGenres", genres);
+        // Содержимое библиотеки, которое надо отобразить
+        Page<BookEntity> pageBooks = bookService.getAll(pageNumber - 1, pageSize, "viewCount", Sort.Direction.ASC);
+        model.addAttribute("pageBooks", pageBooks);
+        // Информация о том, что отображаем (сколько страниц, номер текущей страницы, количество элементов на одной странице и общее количество элементов)
+        maxPageNumber = pageBooks.getTotalPages();
+        totalElements = pageBooks.getTotalElements();
+        model.addAttribute("maxPage", MainPageController.maxPageNumber);
+        model.addAttribute("thisPage", MainPageController.pageNumber);
+        model.addAttribute("pageSize", MainPageController.pageSize);
+        model.addAttribute("totalElements", MainPageController.totalElements);
 
-        return "main/main";
+        return "pages/main";
     }
-
 }
